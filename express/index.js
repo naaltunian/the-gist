ec87c8186eb0163edb5a004633f6e154a1642905
naaltunian <naaltunian@icloud.com>
2018-07-30 16:13:29 -0400

    Server updated but not working correctly with api

--- express/index.js
+++ express/index.js
@@ -1,15 +1,27 @@
 const express = require('express');
 const axios = require('axios');
+const textapi = require('./config');
 
 const app = express();
+
 
 // slide 20 from the first lecture
 app.use(express.static(__dirname + '/the-gist/build'));
 
-app.get('/character/:characterId', async (req, res) => {
- let {data} = await axios.get('http://swapi.co/api/people/'+req.params.characterId).catch((err) => console.log(err));
- res.json(data);
+// partially done
+app.get('/summarize', async (req, res) => {
+  textapi.summarize({
+    url: req.query.url,
+    sentences_percentage: 40
+  },
+  function(error, response) {
+    if(error === null) {
+      res.json(response.sentences);
+    }
+  })
+  res.send("error");
 })
+
 let PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => console.log("Listening on port " + PORT));
--- the-gist/src/Components/Form.js
+++ the-gist/src/Components/Form.js
@@ -1,36 +1,31 @@
 import React from 'react';
 import axios from 'axios';
+// import axios from 'axios';
 
-const APPLICATION_KEY = '64cf462d71cdd6fd69487f59533cb5bc';
-const APPLICATION_ID = '14c0b7bd';
-
-let AYLIENTextAPI = require('aylien_textapi');
-let textapi = new AYLIENTextAPI({
-  application_id: APPLICATION_ID,
-  application_key: APPLICATION_KEY,
-  mode: 'no-cors'
-});
+// const APPLICATION_KEY = '64cf462d71cdd6fd69487f59533cb5bc';
+// const APPLICATION_ID = '14c0b7bd';
+//
+// let AYLIENTextAPI = require('aylien_textapi');
+// let textapi = new AYLIENTextAPI({
+//   application_id: APPLICATION_ID,
+//   application_key: APPLICATION_KEY,
+//   mode: 'no-cors'
+// });
 
 class Form extends React.Component {
 
   constructor(){
-    super()
+    super();
+    this.state = {
+      sentences: []
+    }
   }
 
 
-  Fetchinfo(fetchUrl){
-    textapi.summarize({
-      url: fetchUrl,
-
-    sentences_percentage: 10
-    },
-    function(error, response) {
-      if (error === null) {
-        response.sentences.forEach(function(s) {
-          console.log(s);
-        });
-      }
-    });
+  async fetchInfo(){
+    let fetchUrl = this.refs.url.value;
+    let {data} = await axios.get(`/summarize?url=${fetchUrl}`)
+    this.setState({sentences: data})
   }
 
 
@@ -38,21 +33,24 @@ class Form extends React.Component {
 
   }
 
-handleSubmit = (e) => {
-  if(e) e.preventDefault();
-  const name = this.Fetchinfo(this.refs.url.value);
-  name;
-}
+// handleSubmit = (e) => {
+//   if(e) e.preventDefault();
+//   const name = this.fetchInfo(this.refs.url.value);
+//   name;
+// }
 
   render(){
     return (
       <div className="form">
-
-        <form onSubmit= {this.Fetchinfo.bind(this)} >
+        <form onSubmit={(ev) => {
+            ev.preventDefault();
+            this.fetchInfo();
+          }
+        }>
           <input className="userinput" type="text" ref="url" placeholder="article url here" />
           <br />
-            {this.Fetchinfo()}
-          <input onClick={this.handleSubmit} type="Submit" />
+            {this.state.sentences}
+          <input type="Submit" />
         </form>
 
       </div>
