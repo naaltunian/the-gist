2541cf72a740e3e2a672960c2dcd2d828dd907af
Kais Dewani <kaisdewani@yahoo.com>
2018-07-30 21:57:21 -0400

    'this.props.sentences now showing in correct box'

--- the-gist/src/Components/Summary.js
+++ the-gist/src/Components/Summary.js
@@ -2,24 +2,24 @@ import React from 'react';
 import Form from './Form';
 
 class Summary extends React.Component {
-  render(){
+  render() {
     return (
       <div>
         <div className="form-group">
-
           <label htmlFor="exampleFormControlTextarea1">Here's the Gist</label>
 
-          <section className="form-control" id="exampleFormControlTextarea1" rows="3"></section>
-        </div>
-
-
-        <div className="summary">
-          <section>
-            <h1>{this.props.sentences}</h1>
+          <section
+            className="form-control"
+            id="exampleFormControlTextarea1"
+            rows="3"
+          >
+            {this.props.sentences}
           </section>
         </div>
-    </div>
-    )
+
+        <div className="summary" />
+      </div>
+    );
   }
 }
 
